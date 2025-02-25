d9695964112845cc5c877603a9f85d133697d54c
danilaxila3 <burdigindanil@gmail.com>
2025-02-25 21:58:45 +0500

    Add force to fremove and dremove

--- src/dremove.c
+++ src/dremove.c
@@ -8,11 +8,13 @@
 
 #ifdef _WIN32
 #include <direct.h>
+#include <io.h>
 #define rmdir(path) _rmdir(path)
 #define unlink(path) _unlink(path)
+#define chmod _chmod
 #endif
 
-int delete_dir(const char *path) {
+int delete_dir(const char *path, int force) {
   struct dirent *entry;
   DIR *dir = opendir(path);
 
@@ -33,11 +35,22 @@ int delete_dir(const char *path) {
 
     if (stat(full_path, &statbuf) == 0) {
       if (S_ISDIR(statbuf.st_mode)) {
-        if (delete_dir(full_path) != 0) {
+        if (delete_dir(full_path, force) != 0) {
           closedir(dir);
           return -1;
         }
       } else {
+        if (force) {
+          if (chmod(full_path, 0777) != 0) {
+            fprintf(stderr,
+                    "\e[31m\U0000eec6 \e[0mError modifying permissions of file "
+                    "%s\n",
+                    full_path);
+            closedir(dir);
+            return -1;
+          }
+        }
+
         if (unlink(full_path) != 0) {
           fprintf(stderr, "\e[31m\U0000eec6 \e[0mError unlinking file %s\n",
                   full_path);
@@ -55,6 +68,18 @@ int delete_dir(const char *path) {
 
   closedir(dir);
 
+  if (force) {
+    if (chmod(path, 0777) != 0) {
+      fprintf(stderr,
+              "\e[31m\U0000eec6 \e[0mError modifying permissions of "
+              "directory "
+              "%s\n",
+              path);
+      closedir(dir);
+      return -1;
+    }
+  }
+
   if (rmdir(path) != 0) {
     fprintf(stderr, "\e[31m\U0000eec6 \e[0mError removing directory %s\n",
             path);
@@ -66,14 +91,25 @@ int delete_dir(const char *path) {
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("\e[34m\U0000eec6 \e[0mUsage: dremove <dir1> <dir2> ...\n");
+    printf("\e[34m\U0000eec6 \e[0mUsage: dremove [-f] <dir1> <dir2> ...\n");
     return 1;
   }
 
   int exit_code = 0;
+  int force = 0;
 
   for (int i = 1; i < argc; i++) {
-    if (delete_dir(argv[i]) == 0) {
+    if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--force") == 0) {
+      force = 1;
+    }
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      continue;
+    }
+
+    if (delete_dir(argv[i], force) == 0) {
       printf("\e[32m\U0000eec6 \e[0mRemoved directory %s\n", argv[i]);
     } else {
       fprintf(stderr, "\e[31m\U0000eec6 \e[0mError removing directory %s\n",
--- src/fremove.c
+++ src/fremove.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifdef _WIN32
+#include <io.h>
+#define chmod _chmod
+#endif
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("\e[34m\U0000f05a \e[0mUsage: fremove <file1> <file2> ...\n");
+    printf("\e[34m\U0000f05a \e[0mUsage: fremove [-f] <file1> <file2> ...\n");
     return 1;
   }
 
   int exit_code = 0;
+  int force = 0;
 
   for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--force") == 0) {
+      force = 1;
+    }
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      continue;
+    }
+
+    if (force) {
+      if (chmod(argv[1], 0777) != 0) {
+        fprintf(
+            stderr,
+            "\e[31m\U000f1aa1 \e[0mError modifying permissions of file %s\n",
+            argv[1]);
+        exit_code = 1;
+      }
+    }
+
     if (remove(argv[i]) == 0) {
       printf("\e[32m\U000f1aa1 \e[0mRemoved file %s\n", argv[i]);
     } else {
